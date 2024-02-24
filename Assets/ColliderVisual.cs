643866d442c4e614aecb20cb8e257d06d6ccf29a
Miro Aurela <miroaurela@gmail.com>
2024-02-24 11:57:42 +0200

    Added collider effect for hitting objects to help with spikes.

--- Assets/ColliderVisual.cs
+++ Assets/ColliderVisual.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MissplacedCollider : MonoBehaviour
+public class ColliderVisual : MonoBehaviour
 {
     [SerializeField] Vector3 colliderOffset;
     [SerializeField] float resetTimer;
+    [SerializeField] GameObject colliderVisual;
 
     private Vector3 startLocation;
     private IEnumerator coroutine;
@@ -19,18 +20,20 @@ public class MissplacedCollider : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            transform.position = startLocation + new Vector3(transform.right.x*colliderOffset.x, transform.right.y*colliderOffset.y, transform.right.z*colliderOffset.z);
+            //transform.position = startLocation + new Vector3(transform.right.x*colliderOffset.x, transform.right.y*colliderOffset.y, transform.right.z*colliderOffset.z);
+            colliderVisual.SetActive(true);
             if (coroutine != null) StopCoroutine(coroutine);
-            coroutine = ResetPosition();
+            coroutine = Reset();
             StartCoroutine(coroutine);
         }
     }
 
     //coroutine to reset position after a certain time
-    private IEnumerator ResetPosition()
+    private IEnumerator Reset()
     {
         yield return new WaitForSeconds(resetTimer);
-        transform.position = startLocation;
+        //transform.position = startLocation;
+        colliderVisual.SetActive(false);
     }
 
 }
