2be873ad22d2c2085fd61e5d0b4a1f809cd7c07f
Nina Tepponen <nina.tepponen@gmail.com>
2024-02-24 19:57:18 +0200

    Add finnish flag animation

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -7,6 +7,20 @@ public class Goal : MonoBehaviour
 {
     [SerializeField] string nextLevel;
 
+    public bool UseFinnishFlagSprite = false;
+
+    private void Awake()
+    {
+        if (UseFinnishFlagSprite)
+        {
+            GetComponent<Animator>()?.Play("Flag_Finnish");
+        }
+        else
+        {
+            GetComponent<Animator>()?.Play("Flag_Wave");
+        }
+    }
+
     //if player collides with goal, go to level with certain name
     private void OnTriggerEnter2D(Collider2D collision)
     {
